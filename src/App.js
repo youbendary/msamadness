17f8161ab80cb7914231195fcb427c7d75f22b1e
Youssof <youbendary30@gmail.com>
2023-07-23 16:21:34 -0400

    added react-router and edited app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
+import Home from './components/pages/Home';
+import Events from './components/pages/Events';
+import Volunteer from './components/pages/Volunteer';
+import AboutUs from './components/pages/AboutUs';
+import SignUp from './components/pages/SignUp';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Navbar />
+      <Routes>
+      <Route exact path="/" element={<Home />}/>
+      <Route exact path="/events" element={<Events />}/>
+      <Route exact path="/volunteer" element={<Volunteer />}/>
+      <Route exact path="/about-us" element={<AboutUs />}/>
+      <Route exact path="/sign-up" element={<SignUp />}/>
+      </Routes>
+    </Router>
+    </>
   );
 }
 
