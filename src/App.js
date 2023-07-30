bcc89c8c951b2810f768b781215787cd76e696a0
Mohamed El Ousrouti <melousrouti5@gmail.com>
2023-07-30 12:42:26 -0400

    adding main code to github pages

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import { HashRouter, Routes, Route } from "react-router-dom";
+import './App.css';
+import Home from './components/pages/Home';
+import Events from './components/pages/Events';
+import Volunteer from './components/pages/Volunteer';
+import AboutUs from './components/pages/AboutUs';
+import SignUp from './components/pages/SignUp';
+
+
+function App() {
+  return (
+    <>
+    <HashRouter>
+      <Navbar />
+      <Routes>
+      <Route exact path="/" element={<Home />}/>
+      <Route exact path="/events" element={<Events />}/>
+      <Route exact path="/about-us" element={<AboutUs />}/>
+      <Route exact path="/volunteer" element={<Volunteer />}/>
+      <Route exact path="/sign-up" element={<SignUp />}/>
+      </Routes>
+    </HashRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/pages/Volunteer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import '../../App.css';
+
+export default function Volunteer() {
+    return <h1 className='volunteer'>VOLUNTEER
+    </h1>;
+}
